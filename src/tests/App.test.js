b413fa97660e979aa0158683a0d969c7532b5dfe
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-31 15:48:54 -0300

    iniciado requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+const NUMBER_LINKS = 3;
+
+test('Teste se a aplicação contém um conjunto fixo de três links de navegação.', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /home/i });
+  const linkAbout = screen.getByRole('link', { name: /about/i });
+  const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+  expect(linkHome).toHaveTextContent('Home');
+  expect(linkAbout).toHaveTextContent('About');
+  expect(linkFavorite).toHaveTextContent('Favorite Pokémons');
+});
