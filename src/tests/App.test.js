e4b7aa5f515a93ef6e2b0096c39524fd41039607
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-31 16:48:22 -0300

    correçoes requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,44 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
-const NUMBER_LINKS = 3;
-
 test('Teste se a aplicação contém um conjunto fixo de três links de navegação.', () => {
   renderWithRouter(<App />);
+
   const linkHome = screen.getByRole('link', { name: /home/i });
   const linkAbout = screen.getByRole('link', { name: /about/i });
   const linkFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+
   expect(linkHome).toHaveTextContent('Home');
   expect(linkAbout).toHaveTextContent('About');
   expect(linkFavorite).toHaveTextContent('Favorite Pokémons');
 });
+
+test('teste redirecionamento para a página inicial / , ao clicar no link Home',
+  () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: /home/i });
+    userEvent.click(linkHome);
+    const { pathName } = history.location;
+    expect(pathName).toBe('/');
+  });
+
+test('teste redirecionamento para a página  /abalt , ao clicar no link Abalt',
+  () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbalt = screen.getByRole('link', { name: /abalt/i });
+    userEvent.click(linkAbalt);
+    const { pathName } = history.location;
+    expect(pathName).toBe('/abalt');
+  });
+
+test('teste redirecionamento para página /favorites, no link Pokemons Favoritados',
+  () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorites = screen.getByRole('link', { name: /favorites/i });
+    userEvent.click(linkFavorites);
+    const { pathName } = history.location;
+    expect(pathName).toBe('/favorites');
+  });
