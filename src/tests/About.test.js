102bbd16dbd2cb239e123d7ba169d09964c10153
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2022-01-31 17:31:05 -0300

    iniciado requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,32 @@
-test('', () => {});
-// talvez
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../components';
+
+test('Teste se a página contém as informações sobre a Pokédex.', () => {
+  renderWithRouter(<About />);
+  const informationPokedex = screen.getByText(/This application simulates a Pokédex/i);
+  expect(informationPokedex).toBeInTheDocument();
+});
+
+test('se a página contém um heading h2 com o texto About Pokédex', () => {
+  renderWithRouter(<About />);
+  const title = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+  expect(title).toBeDefined();
+  expect(title).toHaveTextContent('About Pokédex');
+});
+
+test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  renderWithRouter(<About />);
+  const paragraphOne = screen.getByText(/This application simulates a Pokédex, a digital encyclopedia containing all Pokémons/i);
+  expect(paragraphOne).toBeInTheDocument();
+  const paragraphTwo = screen.getByText(/One can filter Pokémons by type, and see more details for each one of them/i);
+  expect(paragraphTwo).toBeInTheDocument();
+});
+
+test('Teste se a página contém a imagem de uma Pokédex', () => {
+  const srcImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  renderWithRouter(<About />);
+  const aboutImg = screen.getByAltText(/Pokédex/i);
+  expect(aboutImg).toHaveAttribute('src', srcImg);
+});
